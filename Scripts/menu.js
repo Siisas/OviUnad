369084e30c46d5d87d64813dcf50a158aba81325
Ricardo Lara <ricardo.lara06@hotmail.com>
2018-10-27 20:00:23 -0500

    Creacion slider, carpetas content

---
+++ Scripts/menu.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+
+    $('#menu').click(function (event) {
+        $('.arrow').slideToggle('slow');
+        $('#gridbox').slideToggle('slow');
+        event.stopPropagation();
+    });
+
+    $('body').click(function () {
+        $('.arrow').hide();
+        $('#gridbox').hide();
+    });
+});
